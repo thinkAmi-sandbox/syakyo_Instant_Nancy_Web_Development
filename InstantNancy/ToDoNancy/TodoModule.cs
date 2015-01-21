89a9ebe1b572f908b2ed89390e9f9b8773265569
thinkAmi <dev.thinkami+git@gmail.com>
2015-01-22 05:40:01 +0900

    No.411 Add GET, POST to TodoModule

--- InstantNancy/ToDoNancy/TodoModule.cs
+++ InstantNancy/ToDoNancy/TodoModule.cs
@@ -5,8 +5,35 @@ using System.Web;
 
 namespace ToDoNancy
 {
-    public class TodoModule
+    using Nancy;
+    using Nancy.ModelBinding;
+
+    public class TodoModule : NancyModule
     {
         public static Dictionary<long, Todo> store = new Dictionary<long, Todo>();
+
+        public TodoModule() : base("todos")
+        {
+            Get["/"] = _ => Response.AsJson(store.Values);
+
+            Post["/"] = _ =>
+            {
+                // TodoオブジェクトをPOSTのBodyにバインディング
+                var newTodo = this.Bind<Todo>();
+
+                if (newTodo.id == 0)
+                {
+                    newTodo.id = store.Count + 1;
+                }
+
+                if (store.ContainsKey(newTodo.id)) return HttpStatusCode.NotAcceptable;
+
+                store.Add(newTodo.id, newTodo);
+
+
+                return Response.AsJson(newTodo)
+                    .WithStatusCode(HttpStatusCode.Created);
+            };
+        }
     }
 }
