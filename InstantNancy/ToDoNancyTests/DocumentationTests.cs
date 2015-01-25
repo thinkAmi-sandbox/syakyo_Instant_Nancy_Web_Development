2c4b54e5593c049404de32fbb170c84eb004fa7d
thinkAmi <dev.thinkami+git@gmail.com>
2015-01-25 14:58:28 +0900

    No.769 Add DocumentationTests in TodoNancyTests project

---
+++ InstantNancy/ToDoNancyTests/DocumentationTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoNancyTests
+{
+    using Nancy;
+    using Nancy.Testing;
+    using Xunit;
+    using ToDoNancy;
+
+    public class DocumentationTests
+    {
+        [Fact]
+        public void Should_give_access_to_overview_documentation()
+        {
+            var sut = new Browser(new Bootstrapper());
+            var actual = sut.Get("/docs/overview.htm", with => with.Accept("text/html"));
+
+            Assert.Equal(HttpStatusCode.OK, actual.StatusCode);
+        }
+    }
+}
