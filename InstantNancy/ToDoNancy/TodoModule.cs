e772786e8be2a17b3843c5101246c4c31433d1b2
thinkAmi <dev.thinkami+git@gmail.com>
2015-01-28 06:01:51 +0900

    No.1050 Update TodosModule, IDataStore, MongoDatastore, inspects the current user

--- InstantNancy/ToDoNancy/TodoModule.cs
+++ InstantNancy/ToDoNancy/TodoModule.cs
@@ -15,19 +15,26 @@ namespace ToDoNancy
 
         public TodoModule(IDataStore todoStore) : base("todos")
         {
-            Get["/"] = _ => 
+            Get["/"] = _ =>
                 Negotiate
                     // `.WithModel(todoStore.GetAll())`だと、例外発生
                     // MongoDB.Driver.MongoCursor`1[ToDoNancy.Todo]' のオブジェクトを型 'ToDoNancy.Todo[]' にキャストできません。
                     // `.WithModel(todoStore.GetAll().ToArray())`でArrayを返すように変更
-                    .WithModel(todoStore.GetAll().ToArray())
-                    //.WithModel(todoStore.GetAll())
+                    //.WithModel(todoStore.GetAll())            // NG
+                    //.WithModel(todoStore.GetAll().ToArray()   // OK
+
+                    // No.1058で以下のように修正が入ると、エラーにならない
+                    .WithModel(todoStore.GetAll()
+                        .Where(todo => todo.userName == Context.CurrentUser.UserName)
+                        .ToArray())
+
                     .WithView("Todos");
 
             Post["/"] = _ =>
             {
                 // TodoオブジェクトをPOSTのBodyにバインディング
                 var newTodo = this.Bind<Todo>();
+                newTodo.userName = Context.CurrentUser.UserName;
 
                 if (newTodo.id == 0)
                 {
@@ -46,14 +53,20 @@ namespace ToDoNancy
             {
                 var updatedTodo = this.Bind<Todo>();
 
-                if (!todoStore.TryUpdate(updatedTodo)) return HttpStatusCode.NotFound;
-
+                if (!todoStore.TryUpdate(updatedTodo, Context.CurrentUser.UserName))
+                {
+                    return HttpStatusCode.NotFound;
+                }
+                
                 return updatedTodo;
             };
 
             Delete["/{id}/"] = p =>
             {
-                if (!todoStore.TryRemove(p.id)) return HttpStatusCode.NotFound;
+                if (!todoStore.TryRemove(p.id, Context.CurrentUser.UserName))
+                {
+                    return HttpStatusCode.NotFound;
+                }
 
                 return HttpStatusCode.OK;
             };
