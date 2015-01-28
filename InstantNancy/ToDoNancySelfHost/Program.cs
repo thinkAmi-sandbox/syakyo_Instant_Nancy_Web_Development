f4542a3a20f03e3e0e37dceb9e338985a0264d7b
thinkAmi <dev.thinkami+git@gmail.com>
2015-01-29 05:28:42 +0900

    No.1119-11 Add NancyHost code in program.cs

--- InstantNancy/ToDoNancySelfHost/Program.cs
+++ InstantNancy/ToDoNancySelfHost/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ToDoNancySelfHost
 {
+    using Nancy.Hosting.Self;
+    using ToDoNancy;
+        
     class Program
     {
         static void Main(string[] args)
         {
+            TodoModule artificiaReference;
+            var nancyHost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:8080"));
+            nancyHost.Start();
+
+            Console.ReadKey();
+
+            nancyHost.Stop();
         }
     }
 }
